494efc5ada1e09961a4b20659e49e4362f2e330c
CactusCata <adam.chareyre.1999@gmail.com>
2022-09-21 16:18:39 +0200

    t

--- I53-Theorie_du_language_et_compilation/TP/TP2/Exo/lexiconPart.hpp
+++ I53-Theorie_du_language_et_compilation/TP/TP2/Exo/lexiconPart.hpp
@@ -1,6 +1,14 @@
 #ifndef LEXICON_PART_HPP
 #define LEXICON_TYPE_HPP
 
+class LexiconPart {
 
+    public:
+
+        LexiconPart(const std::string& lexiconTypeName, const char& value);
+
+
+
+};
 
 #endif
