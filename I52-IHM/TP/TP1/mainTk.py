c4c10a7dec2014056994ae90e2d57970450f48ea
CactusCata <adam.chareyre.1999@gmail.com>
2022-11-18 06:34:57 +0100

    I52IHM - TP1

--- I52-IHM/TP/TP1/mainTk.py
+++ I52-IHM/TP/TP1/mainTk.py
@@ -8,7 +8,10 @@ class MainTk:
         self.colors = colors
         self.sortedRGB = sortedRGB
 
-        self.root = Tk()
+        self.mainRoot = Tk()
+
+
+        self.root = Toplevel(self.mainRoot)
         rootDimensions = setupRootGeometry(self.root, ratio=0.3)
         self.root.minsize(rootDimensions[0], rootDimensions[1])
         self.root.maxsize(rootDimensions[0], rootDimensions[1])
@@ -19,31 +22,35 @@ class MainTk:
         self.labelColorSelected = Label(self.root, textvariable=self.colorSelected)
         self.labelColorSelected.pack()
 
-        self.matrixCanvasDimension = (int(0.8 * rootDimensions[0]), int(0.8 * rootDimensions[1]))
-        self.matrixCanvas = Canvas(self.root, width=self.matrixCanvasDimension[0], height=self.matrixCanvasDimension[1], bg="white")
-        #self.matrixCanvas.configure(scrollregion=self.matrixCanvas.bbox("all")) #
-        self.matrixCanvas.pack()
+        self.frameMatrix = Frame(self.root)
+        self.frameMatrix.pack(expand=True, fill="both")
+
+        self.matrixCanvasDimension = (int(0.97 * rootDimensions[0]), int(0.9 * rootDimensions[1]))
+        self.matrixCanvas = Canvas(self.frameMatrix, width=self.matrixCanvasDimension[0], height=self.matrixCanvasDimension[1], bg="white")
+        self.matrixCanvas.pack(side="left", fill="y")
 
         self.setupMatrixCanvas()
 
-        self.matrixCanvasScrollbar = Scrollbar(command=self.matrixCanvas.yview)
+        self.matrixCanvasScrollbar = Scrollbar(self.frameMatrix, command=self.matrixCanvas.yview)
         self.matrixCanvasScrollbar.pack(side="right", fill="y")
+        self.matrixCanvas.configure(scrollregion=self.matrixCanvas.bbox("all"))
         self.matrixCanvas.configure(yscrollcommand=self.matrixCanvasScrollbar.set)
+
 
         self.buttonsFrame = Frame(self.root)
         self.buttonsFrame.pack()
 
         self.buttonOK = Button(self.buttonsFrame, text="OK", command=lambda:self.okButton())
-        self.buttonOK.pack()
+        self.buttonOK.pack(side="left")
 
         self.buttonCancel = Button(self.buttonsFrame, text="Annuler", command=lambda:self.cancelButton())
-        self.buttonCancel.pack()
+        self.buttonCancel.pack(side="left")
 
         self.root.mainloop()
 
-    def setupMatrixCanvas(self, rectSizePx=15, spaceBetweenRects=10):
-        startDrawPxLeft = 3
-        startDrawPxUp = 3
+    def setupMatrixCanvas(self, rectSizePx=30, spaceBetweenRects=10):
+        startDrawPxLeft = spaceBetweenRects
+        startDrawPxUp = spaceBetweenRects
         rectPerLine = self.matrixCanvasDimension[0] // (rectSizePx + spaceBetweenRects)
         rectLineAmount = (len(self.sortedRGB) // rectPerLine) + 1
         for i in range(rectLineAmount):
@@ -53,12 +60,22 @@ class MainTk:
                     currentRGB = self.colors[colorName]
                     leftUpCornerCoordinatesPx = (startDrawPxLeft + j * (rectSizePx + spaceBetweenRects), startDrawPxUp + i * (rectSizePx + spaceBetweenRects))
                     self.matrixCanvas.create_rectangle(leftUpCornerCoordinatesPx[0], leftUpCornerCoordinatesPx[1], leftUpCornerCoordinatesPx[0] + rectSizePx, leftUpCornerCoordinatesPx[1] + rectSizePx, fill=utils.rgbToHex(currentRGB), tags=("colorRect", colorName))
+                    self.matrixCanvas.tag_bind(colorName, "<Button-1>", lambda c:self.couleurPresse(c))
 
     def okButton(self):
-        print("Bouton ok préssé !")
+        currentColorName = self.colorSelected.get()
+        if (currentColorName != ""):
+            currentRGBColor = self.colors[currentColorName]
+            self.mainRoot.configure(bg=utils.rgbToHex(currentRGBColor))
+        
+        self.root.destroy()
 
     def cancelButton(self):
-        print("Bouton annuler préssé !")
+        self.root.destroy()
+
+    def couleurPresse(self, event=None):
+        id = self.matrixCanvas.find_withtag("current")
+        self.colorSelected.set(self.matrixCanvas.gettags(id)[1])
 
 
 def setupRootGeometry(root, ratio=0.5):
